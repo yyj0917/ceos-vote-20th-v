006a2e75761863cb6475c8411f879a0c4a0c9b25
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-01-03 17:28:59 +0900

    Feat: 파트 투표, 결과 조회 API 구현

--- frontend/src/lib/api/vote-part.ts
+++ frontend/src/lib/api/vote-part.ts
@@ -1,5 +1,7 @@
 import axiosConfig from "./config/axiosConfig";
 
+// 이름은 다 part로 통일하고 따로 프백 호출
+
 // GET : 파트 후보 조회
 export async function getPartList(part: "FRONT" | "BACK") {
     if (!["FRONT", "BACK"].includes(part)) {
@@ -17,38 +19,42 @@ export async function getPartList(part: "FRONT" | "BACK") {
     }
 }
 
-{/*
+// POST : 파트 투표하기
+export async function postPartVote(part: "FRONT"|"BACK", partname: string) {
+    if (!["FRONT", "BACK"].includes(part)) {
+        throw new Error("유효하지 않은 파트입니다.");
+    }
 
-    
-// 팀 투표 post api
-export async function postTeamVote(teamname: string) {
-  try {
+    try {
     const response = await axiosConfig.post("/votes", {}, {
         params: {
-            type: "team",
-            voted: teamname,
+            type: "leader",
+            part,
+            voted: partname,
+            // voted 맞는지 노션이랑 체크하기
         }
     });
     return response.data;
-  } catch (error: any) {
+    }   catch (error: any) {
     throw error;
-  }
+    }
 };
 
-// 팀 투표 결과 조회 get api
-export async function getTeamVoteResult() {
-  try {
-    const response = await axiosConfig.get("/api/votes/results", {
-      params: {
-        type: "team",
-      }
+// GET : 파트 투표 결과 조회
+export async function getPartVoteResult(part: "FRONT"|"BACK") {    if (!["FRONT", "BACK"].includes(part)) {
+    throw new Error("유효하지 않은 파트입니다.");
+    // union type 사용해서 유용한 part 선언했는지 체크
+    }
+
+    try {
+        const response = await axiosConfig.get("/api/votes/results", {
+        params: {
+            type: "leader",
+            part
+        }
     });
     return response.data;
-  } catch (error: any) {
+    }   catch (error: any) {
     throw error;
-  }
+    }
 };
-
-
-
-    */}
